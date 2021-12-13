cf863446be5858a68285e264a34bafc1587e4b2e
Anushka Pillay <91051145+theoddnari@users.noreply.github.com>
2021-12-13 20:06:37 +0530

    project

---
+++ CA1projects/mode.js
@@ -0,0 +1,20 @@
+   function darkMode() {
+          var element = document.body;
+          var content = document.getElementById("DarkModetext");
+          element.className = "dark-mode";
+        
+        }
+        function lightMode() {
+          var element = document.body;
+          var content = document.getElementById("DarkModetext");
+          element.className = "light-mode";
+          
+        }
+
+        $(document).ready(function(){
+          $("button").click(function(){
+            $(".remove").remove();
+          });
+        });
+
+     
